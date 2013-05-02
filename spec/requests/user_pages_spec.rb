6f35e2d3b5cced4b7a77d7d16342a7a9d92f5096
Chris Fahmy <chrisfahmy@hotmail.com>
2013-05-02 09:53:26 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,11 +12,11 @@ describe "User pages" do
   end
 
   describe "profile page" do
-  let(:user) { FactoryGirl.create(:user) }
-  before { visit user_path(user) }
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-  it { should have_selector('h1',    text: user.name) }
-  it { should have_selector('title', text: user.name) }
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
   end
 require 'spec_helper'
 
@@ -40,6 +40,14 @@ require 'spec_helper'
         fill_in "Confirmation", with: "foobar"
       end
 
+      describe "after saving the user" do
+      	before { click_button submit }
+      	let(:user) { User.find_by_email('user@example.com') }
+
+      	it{ should have_selector('title', text: user.name) }
+      	it{ should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+	end
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
